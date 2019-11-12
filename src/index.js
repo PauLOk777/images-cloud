c7c075e06eafe176952490d0a732708563f09f37
PauLOk777 <trocjuk2000@gmail.com>
2019-11-13 01:44:32 +0200

    Modified index.js just a little changes, create main page(index.html), then wrote some code and made some styles.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.use('/static', express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
     res.end();
-}).listen(3000);
+}).listen(process.env.PORT || 3000);
