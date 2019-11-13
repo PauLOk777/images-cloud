e696b00ddcc9b25f53a6b50eb30926d92076b69c
PauLOk777 <trocjuk2000@gmail.com>
2019-11-14 00:22:13 +0200

    Added new pages: login, registration and rules. Then modified main page, added new handlers in index.js, then added some new styles in stylesheets.css.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,20 @@ const path = require('path');
 
 const app = express();
 
-app.use('/static', express.static('public'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-    res.end();
+	res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 }).listen(process.env.PORT || 3000);
+
+app.get('/login', (req, res) => {
+	res.sendFile(path.join(__dirname, '..', 'public', 'login.html'));
+});
+
+app.post('/login/submit', (req, res) => {
+	res.send('Your account must be here!');
+});
+
+app.get('/rules', (req, res) => {
+res.sendFile(path.join(__dirname, '..', 'public', 'rules.html'))
+});
