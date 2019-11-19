4d6f08dab16ed5e6bf79330a2d4b52b026a2f0a0
PauLOk777 <trocjuk2000@gmail.com>
2019-11-19 14:52:29 +0200

    Now we use standardization for our website, create folder views with our html pages and layouts, changed index.js.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,44 @@
 const express = require('express');
+const hbs = require('hbs');
+const expressHbs = require('express-handlebars');
 const path = require('path');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.engine("hbs", expressHbs(
+    {
+        layoutsDir: "views/layouts", 
+        defaultLayout: "layout",
+        extname: "hbs"
+    }
+));
+
+hbs.registerPartials(path.join(__dirname, '..', 'views', 'partials'));
+
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-}).listen(process.env.PORT || 3000);
-
-app.get('/login', (req, res) => {
-	res.sendFile(path.join(__dirname, '..', 'public', 'login.html'));
-});
+app.set('view engine', 'hbs');
 
 app.post('/login/submit', (req, res) => {
-	res.send('Your account must be here!');
+    res.send('Your account must be here!');
+});
+
+app.get('/login', (req, res) => {
+	res.render('login.hbs', { title: 'Login' });
 });
 
 app.get('/rules', (req, res) => {
-res.sendFile(path.join(__dirname, '..', 'public', 'rules.html'))
+	res.render('rules.hbs', { title: 'Rules' });
 });
+
+app.get('/registration', (req, res) => {
+	res.render('registration.hbs', { title: 'Registration' });
+});
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', { title: 'Home' });
+});
+
+app.listen(PORT);
